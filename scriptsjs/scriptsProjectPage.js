2005deb319c56726d6c47e69a534bd6ecaf20d4c
Josen190 <92809532+Josen190@users.noreply.github.com>
2021-10-26 22:44:08 +0400

    доделал окно добавить колонку

--- scriptsjs/scriptsProjectPage.js
+++ scriptsjs/scriptsProjectPage.js
@@ -1,12 +1,31 @@
 function SaveStatusFunction(){
-    
-    var radios = document.getElementsByName('flexRadioStatus');
+
+    let radios = document.getElementsByName('flexRadioStatus');
 
     for (var i = 0, length = radios.length; i < length; i++) {
       if (radios[i].checked) {
         document.getElementById('inputStatus').value = radios[i].value;
-    
+
         break;
       }
     }
+}
+
+
+function hasClass(ele,cls) {
+  return !!ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
+}
+
+
+function addClass(id,cls) {
+  ele = document.getElementById(id);
+  if (!hasClass(ele,cls)) ele.className += " "+cls;
+}
+
+function removeClass(id,cls) {
+  ele = document.getElementById(id);
+  if (hasClass(ele,cls)) {
+    var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
+    ele.className=ele.className.replace(reg,' ');
+  }
 }
