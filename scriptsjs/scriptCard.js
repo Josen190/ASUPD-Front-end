02c728130dc10dec4e2830717e003813338dd4f2
Josen190 <92809532+Josen190@users.noreply.github.com>
2021-11-01 12:32:38 +0400

    готово

--- scriptsjs/scriptCard.js
+++ scriptsjs/scriptCard.js
@@ -1,8 +1,59 @@
-function addComent(){
+
+function lastEdit(userId = 'тест', data = '01.11.2021') {
+  document.getElementById("lastData").innerHTML = "Последнее изменение: " + data;
+  document.getElementById("lastUser").innerHTML = "Изменил: " + userId;
+}
+
+
+function newComentHTML(text, userId = 'тест', data = '01.11.2021'){
   if ('content' in document.createElement('template')) {
     let template = document.getElementById("tempComment");
     let clone = template.content.cloneNode(true);
+
+    clone.getElementById("userCommen").innerHTML = userId;
+    clone.getElementById("dataComment").innerHTML = data;
+    clone.getElementById("contentComment").innerHTML = text;
+
     let listComent = document.getElementById("commentList");
     listComent.appendChild(clone);
+
+    lastEdit(userId, data);
   }
+}
+
+function addComent(parent){
+  let text = parent.getElementsByTagName('textarea')[0];
+
+
+  if (text.value == ""){
+    return;
+  }
+
+  newComentHTML(text.value);
+
+  text.value = "";
+}
+
+
+
+function eidtContentCard(parent) {
+  let btn = parent.querySelector(".mode").childNodes;
+  btn[1].removeAttribute('disabled');
+  btn[3].setAttribute('disabled', true);
+
+  content = parent.querySelector('.filling');
+  text = content.getElementsByTagName('textarea')[0];
+  text.removeAttribute('disabled');
+
+}
+
+function viewingContentCard(parent) {
+  let btn = parent.querySelector(".mode").childNodes;
+  btn[1].setAttribute('disabled', true);
+  btn[3].removeAttribute('disabled');
+
+  content = parent.querySelector('.filling');
+  text = content.getElementsByTagName('textarea')[0];
+  text.setAttribute('disabled', true);
+
 }
