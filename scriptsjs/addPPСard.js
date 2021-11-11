a2f0524ca91d09e0f1a9f160bbadf533c3c5419c
Josen190 <92809532+Josen190@users.noreply.github.com>
2021-11-11 16:36:17 +0400

    +

---
+++ scriptsjs/addPPСard.js
@@ -0,0 +1,18 @@
+async function addPPCard(token) {
+    fetch("localhost:8080/project_proposal/get_all", token)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Ошибочный запрос');
+        }
+        return response.json();
+      })
+      .catch(error => {
+        console.log('error', error);
+        alert('Нет токена');
+        document.location.href = "authorization.html"
+      });
+  }
+  function logOut(){
+    sessionStorage.removeItem('token');
+    document.location.href = "authorization.html";
+  }
