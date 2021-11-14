8c6dec5d50f2a3384617d33ca07b0dc7c7d5ec2e
DmitryZero <75453564+DmitryZero@users.noreply.github.com>
2021-11-15 03:51:15 +0400

    Частично сделал связку с бек-эндом в банке предложений

--- scriptsjs/fetchScripts.js
+++ scriptsjs/fetchScripts.js
@@ -1,53 +1,170 @@
 async function checkToken() {
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", sessionStorage.getItem('token'));
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", sessionStorage.getItem('token'));
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
 
-    fetch("http://localhost:8080/user/self", requestOptions)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Ошибочный запрос');
-            }
-            return response.json();
-        })
-        .catch(error => {
-            console.log('error', error);
-            alert('Нет токена');
-            document.location.href = "authorization.html"
-        });
+  const status = await fetch("http://localhost:8080/user/self", requestOptions)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Ошибочный запрос');
+      }
+      return true;
+    })
+    .catch(error => {
+      console.log('error', error);
+      alert('Нет токена');
+      return false;
+    });
+  return status;
 }
 
 //Загружает информацию о пользователе на странице личного кабинета
 async function LoadInformationOfUser() {
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", sessionStorage.getItem('token'));
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", sessionStorage.getItem('token'));
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
 
-    fetch("http://localhost:8080/user/self", requestOptions)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Ошибочный запрос');
-        }
-        return response.json();
-      })
-      .then((result) => {
-        document.getElementById('full_name').innerText =  result['lastName'] + ' ' + result['firstName'] + ' ' + result['patronymic'];
-        document.getElementById('work_place').innerText += ': ' + result['workPlace'];
-        document.getElementById('status').innerText += ': ' + result['status'];
-        document.getElementById('birthday').innerText += ': ' + result['birthDate'];
-        document.getElementById('phone').innerText += ': ' + result['phoneNumber'];
-      })
-      .catch(error => {
-        console.log('error', error);
-        alert('Нет токена');
-        document.location.href = "authorization.html"
-      });
-  }
+  fetch("http://localhost:8080/user/self", requestOptions)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Ошибочный запрос');
+      }
+      return response.json();
+    })
+    .then((result) => {
+      document.getElementById('full_name').innerText = result['lastName'] + ' ' + result['firstName'] + ' ' + result['patronymic'];
+      document.getElementById('work_place').innerText += ': ' + result['workPlace'];
+      document.getElementById('status').innerText += ': ' + result['status'];
+      document.getElementById('birthday').innerText += ': ' + result['birthDate'];
+      document.getElementById('phone').innerText += ': ' + result['phoneNumber'];
+    })
+    .catch(error => {
+      console.log('error', error);
+      alert('Ошибка LoadInformationOfUser');
+    });
+}
+//Загружает информацию о проектах на странице личного кабинета
+async function LoadAllProjectsOfUser() {
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", sessionStorage.getItem('token'));
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  fetch("http://localhost:8080/user/self", requestOptions)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Ошибочный запрос');
+      }
+      return response.json();
+    })
+    .then((result) => {
+      let container = document.querySelector("#projects-list");
+      for (var i = 0; i < Object.keys(result['projectUuidList']).length; i++) {
+        let liElement = document.createElement('li');
+        liElement.insertAdjacentHTML('beforeend', '<a href="project.html">Проект</a>');
+        container.appendChild(liElement);
+      }
+    })
+    .catch(error => {
+      console.log('error', error);
+      alert('Ошибка LoadAllProjectsOfUser');
+    });
+}
+
+//Загружает предложения в банке проектов 
+async function LoadAllProposals() {
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", sessionStorage.getItem('token'));
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  await fetch("http://localhost:8080/project_proposal/get_all", requestOptions)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Ошибочный запрос');
+      }
+      return response.json();
+    })
+    .then(async (result) => {
+      let proposalsArray = Object.keys(result).length;
+      for (var i = 0; i < Object.keys(result).length; i++) {
+        await LoadInformationAboutProposal(result[i]).then((response) => {
+          var k = newProposalCard(response['name'], response['id']);
+        });
+      }
+    })
+    .catch(error => {
+      console.log('error', error);
+      alert('Ошибка LoadAllProposals');
+    });
+}
+
+async function LoadInformationAboutProposal(tokenOfProposal) {
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", sessionStorage.getItem('token'));
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  const response = await fetch("http://localhost:8080/project_proposal/" + tokenOfProposal, requestOptions)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Ошибочный запрос');
+      }
+      return response.json();
+    })
+    .then((result) => {
+      return result;
+    })
+    .catch(error => {
+      console.log('error', error);
+      alert('Ошибка LoadInformationAboutProposal');
+    });
+  return response;
+}
+
+async function CreateProjectFromProposal(proposalId, managerId) {
+  var myHeaders = new Headers();
+  myHeaders.append("managerId", managerId);
+  myHeaders.append("Authorization", sessionStorage.getItem('token'));
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  fetch("http://localhost:8080/project_proposal/" + proposalId, requestOptions)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Ошибочный запрос');
+    }
+    return response.json();
+  })
+  .then((result) => {
+    document.location.href = "account.html";
+  })
+  .catch(error => {
+    console.log('error', error);
+    alert('Ошибка LoadInformationAboutProposal');
+  });
+}
--- scriptsjs/scriptProjectBank.js
+++ scriptsjs/scriptProjectBank.js
@@ -1,11 +1,17 @@
+function setAttributes(el, attrs) {
+    for (var key in attrs) {
+        el.setAttribute(key, attrs[key]);
+    }
+}
+
 let bank = document.getElementById("bank");
 let loadCircle = document.getElementById("load_circle");
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max);
 }
 
-function removeAllChild(myNode){
+function removeAllChild(myNode) {
     while (myNode.firstChild) {
         myNode.removeChild(myNode.firstChild);
     }
@@ -14,8 +20,8 @@ function removeAllChild(myNode){
 function setOpacity(el, time) {
     let op = 0;
     while (op <= 1) {
-        (function(_op) {
-            setTimeout(function() { el.style.opacity = _op; }, time + op * 600);
+        (function (_op) {
+            setTimeout(function () { el.style.opacity = _op; }, time + op * 600);
         })(op);
         op += 0.1;
     }
@@ -23,58 +29,58 @@ function setOpacity(el, time) {
 
 let projectProposal = document.getElementById("project-proposal");
 let form = projectProposal.querySelectorAll(".form");
-function modalFormProject(projectID = "project_card"){
-//    //добовляем имя
-//    form.name.querySelector("h3").innerHTML = name;
-//    //добовляем основную иинформацию
-//    form.basic-info.arrDiv[i].innerHTML = info;
-//
-//    //доболяем стадии
-//    let arrStagesHtml = form.stages.querySelector("dl");
-//    removeAllChild(arrStagesHtml); //удаляем предыдущие стадии
-//    for (let i = 0; i < arrStages.length; i++){
-//        let stagesHtml = document.createElement('dt');
-//        stagesHtml.innerHTML = arrStages[i];
-//        arrStagesHtml.appendChild(stagesHtml);
-//    }
-//
-//    //добовляем руговодителей
-//    let arrSupervisorsHtml = form.s-m.supervisors.querySelector("dl");
-//    removeAllChild(arrSupervisorsHtml);//удаляем предыдущих руководителей
-//    for (let i = 0; i < arrSupervisors.length; i++){
-//        let supervisorsHtml = document.createElement('dt');
-//        supervisorsHtml.innerHTML = arrSupervisors[i];
-//        arrSupervisorsHtml.appendChild(supervisorsHtml);
-//    }
-//
-//    //добовляем наставников
-//    let arrMentorsHtml = form.s-m.mentors.querySelector("dl");
-//    removeAllChild(arrMentorsHtml);//удаляем предыдущих наставников
-//    for (let i = 0; i < arrMentors.length; i++){
-//        let mentorsHtml = document.createElement('dt');
-//        mentorsHtml.innerHTML = arrMentors[i];
-//        arrMentorsHtml.appendChild(mentorsHtml);
-//    }
+function modalFormProject(projectID = "project_card") {
+    //    //добовляем имя
+    //    form.name.querySelector("h3").innerHTML = name;
+    //    //добовляем основную иинформацию
+    //    form.basic-info.arrDiv[i].innerHTML = info;
+    //
+    //    //доболяем стадии
+    //    let arrStagesHtml = form.stages.querySelector("dl");
+    //    removeAllChild(arrStagesHtml); //удаляем предыдущие стадии
+    //    for (let i = 0; i < arrStages.length; i++){
+    //        let stagesHtml = document.createElement('dt');
+    //        stagesHtml.innerHTML = arrStages[i];
+    //        arrStagesHtml.appendChild(stagesHtml);
+    //    }
+    //
+    //    //добовляем руговодителей
+    //    let arrSupervisorsHtml = form.s-m.supervisors.querySelector("dl");
+    //    removeAllChild(arrSupervisorsHtml);//удаляем предыдущих руководителей
+    //    for (let i = 0; i < arrSupervisors.length; i++){
+    //        let supervisorsHtml = document.createElement('dt');
+    //        supervisorsHtml.innerHTML = arrSupervisors[i];
+    //        arrSupervisorsHtml.appendChild(supervisorsHtml);
+    //    }
+    //
+    //    //добовляем наставников
+    //    let arrMentorsHtml = form.s-m.mentors.querySelector("dl");
+    //    removeAllChild(arrMentorsHtml);//удаляем предыдущих наставников
+    //    for (let i = 0; i < arrMentors.length; i++){
+    //        let mentorsHtml = document.createElement('dt');
+    //        mentorsHtml.innerHTML = arrMentors[i];
+    //        arrMentorsHtml.appendChild(mentorsHtml);
+    //    }
 
     let arrDiv = form.querySelectorAll("div");
-    for (let i = 0; i < arrDiv.length; i++){
-        if (arrDiv[i].className == "name"){
+    for (let i = 0; i < arrDiv.length; i++) {
+        if (arrDiv[i].className == "name") {
             arrDiv[i].querySelector("h3").innerHTML = name;
-        }else if(arrDiv[i].className == "basic-info") {
+        } else if (arrDiv[i].className == "basic-info") {
             arrDiv[i].innerHTML = info;
-        }else if(arrDiv[i].className == "stages") {
+        } else if (arrDiv[i].className == "stages") {
             let arrStagesHtml = arrDiv[i].querySelector("dl");
             removeAllChild(arrStagesHtml);
-            for (let i = 0; i < arrStages.length; i++){
+            for (let i = 0; i < arrStages.length; i++) {
                 let stagesHtml = document.createElement('dt');
                 stagesHtml.innerHTML = arrStages[i];
                 arrStagesHtml.appendChild(stagesHtml);
             }
-        }else if(arrDiv[i].className == "s-m") {
+        } else if (arrDiv[i].className == "s-m") {
             let supervisors = arrDiv[i].querySelector(".supervisors");
             let arrSupervisorsHtml = arrDiv[i].querySelector("dl");
             removeAllChild(arrSupervisorsHtml);
-            for (let i = 0; i < arrSupervisors.length; i++){
+            for (let i = 0; i < arrSupervisors.length; i++) {
                 let supervisorsHtml = document.createElement('dt');
                 supervisorsHtml.innerHTML = arrSupervisors[i];
                 arrSupervisorsHtml.appendChild(supervisorsHtml);
@@ -82,7 +88,7 @@ function modalFormProject(projectID = "project_card"){
             let mentors = arrDiv[i].querySelector(".mentors");
             let arrMentorsHtml = arrDiv[i].querySelector("dl");
             removeAllChild(arrMentorsHtml);
-            for (let i = 0; i < arrMentors.length; i++){
+            for (let i = 0; i < arrMentors.length; i++) {
                 let mentorsHtml = document.createElement('dt');
                 mentorsHtml.innerHTML = arrMentors[i];
                 arrMentorsHtml.appendChild(mentorsHtml);
@@ -91,41 +97,69 @@ function modalFormProject(projectID = "project_card"){
     }
 }
 
-function newProjectCard(projectID = "project_card", name = "Новый проект"){
-  if ('content' in document.createElement('template')) {
-    let template = document.getElementById("temp_project_card");
-    let clone = template.content.cloneNode(true);
+// function newProposalCard(name = "Новый проект", uuid=""){
+//   if ('content' in document.createElement('template')) {
+//     let template = document.getElementById("temp_project_card");
+//     let clone = template.content.cloneNode(true);
 
-    clone.querySelector("p").innerHTML = name;
-    clone.querySelector("img").src = "../img/Bg_PB/" + (1 + getRandomInt(6)) + ".jpg";
-    clone.id = projectID;
-    clone.querySelector(".listen").addEventListener("click", function(){
-              //loadCircle.removeAttribute("style");
-//              modalFormProject(projectID);
-//              loadCircle.setAttribute("style", "display: none");
-//              projectProposal.removeAttribute("style");
-              setTimeout(() => {
-                        loadCircle.setAttribute("style", "display: none");
-                        alert("Error");
-                        projectProposal.removeAttribute("style");
-                        setOpacity(projectProposal, 100);
-                    }, 2000);
-          });
+//     clone.querySelector("p").innerHTML = name;
+//     clone.querySelector("img").src = "../img/Bg_PB/" + (1 + getRandomInt(6)) + ".jpg";
+//     clone.querySelector("#testProject").dataset.uuid = uuid;
+//     clone.querySelector(".listen").addEventListener("click", async function(){
+//               loadCircle.removeAttribute("style");
+//               let el = clone.querySelector("#testProject").dataset.uuid;
+// //              modalFormProject(projectID);
+// //              loadCircle.setAttribute("style", "display: none");
+// //              projectProposal.removeAttribute("style");
+//               setTimeout(() => {
+//                         loadCircle.setAttribute("style", "display: none");
+//                         projectProposal.removeAttribute("style");
+//                         setOpacity(projectProposal, 100);
+//                     }, 2000);
+//           });
 
-    bank.appendChild(clone);
-  }
+//     bank.appendChild(clone);
+//   }
+// }
+
+function newProposalCard(name = "Новый проект", uuid = "") {
+    var proposal_icon_div = document.createElement('div');
+    setAttributes(proposal_icon_div, { "id": "testProject", "class": "project-card", "data-uuid": uuid });    
+    proposal_icon_div.insertAdjacentHTML('beforeend',
+        '<div class="info">' +
+            '<img src = "../img/Bg_PB/1.jpg">' +
+        '</div>' +
+        '<div class="name">' +
+            '<p ib="name"></p>' +
+        '</div>');
+    proposal_icon_div.querySelector("p").innerHTML = name;
+    proposal_icon_div.querySelector("img").src = "../img/Bg_PB/" + (1 + getRandomInt(6)) + ".jpg";
+    proposal_icon_div.addEventListener('click', async () => {
+        loadCircle.removeAttribute("style");        
+
+        var informationAboutProject = await LoadInformationAboutProposal(proposal_icon_div.getAttribute('data-uuid'));
+        document.querySelector('#project-proposal').dataset.uuidOfProposalWindow = informationAboutProject['id'];
+        document.querySelector('#nameOfProposal').textContent = informationAboutProject['name'];
+        document.querySelector('#basicInfoAboutProposal').textContent = informationAboutProject['information'];
+        document.querySelector('#supervisors').textContent = informationAboutProject['projectManagersUuidList'][0];
+
+        setTimeout(() => {
+            loadCircle.setAttribute("style", "display: none");
+            projectProposal.removeAttribute("style");
+            setOpacity(projectProposal, 100);
+        }, 2000);
+    })
+    bank.appendChild(proposal_icon_div);
 }
 
+// document.addEventListener("click", function (e) {
+//     console.log(e.target);
+//     if (e.target.type == "button") {
+//         if (e.target.name == "close") {
+//             projectProposal.setAttribute("style", "display: none");
+//         } else if (e.target.name == "create") {
+//             alert("Error");
+//         }
 
-
-document.addEventListener("click", function(e) {
-  console.log(e.target);
-  if (e.target.type == "button") {
-    if(e.target.name == "close"){
-      projectProposal.setAttribute("style", "display: none");
-    }else if(e.target.name == "create"){
-      alert("Error");
-    }
-
-  }
-});
+//     }
+// });
